265d565eb0b0dc1a86eacdad899e21ef4bee545a
Andre Gomes <andregomes.ronfe@gmail.com>
2024-11-22 11:50:57 +0000

    application and element viwer

--- gauntlet/src/main/java/ldts/t09g06/Application.java
+++ gauntlet/src/main/java/ldts/t09g06/Application.java
@@ -8,11 +8,15 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import ldts.t09g06.gui.LanternaGUI;
 
+import java.awt.*;
 import java.io.IOException;
+
+import static ldts.t09g06.model.Constants.HEIGHT;
+import static ldts.t09g06.model.Constants.WIDTH;
+
 public class Application {
-    public static void main(String[] args) throws IOException {
-        LanternaGUI gui = new LanternaGUI();
-        Game game = new Game();
-        game.run();
+    public static void main(String[] args) throws IOException, FontFormatException {
+        LanternaGUI gui = new LanternaGUI(WIDTH, HEIGHT);
+        gui.
     }
 }
--- gauntlet/src/main/java/ldts/t09g06/view/ElementViewer.java
+++ gauntlet/src/main/java/ldts/t09g06/view/ElementViewer.java
@@ -1,7 +1,12 @@
 package ldts.t09g06.view;
 
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import ldts.t09g06.Element;
 import ldts.t09g06.gui.LanternaGUI;
 
-public interface ElementViewer <T extends Element> {
-    void draw(T element, LanternaGUI gui);
+public class ElementViewer (LanternaGUI gui, Element e){
+
 }
