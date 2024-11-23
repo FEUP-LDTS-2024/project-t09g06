fd0680970e302ca406b56b1e5956684d8920e36a
Tiago <tiagofccarvalho13@gmail.com>
2024-11-23 15:42:33 +0000

    fixed an error in AmmoViewer

--- gauntlet/src/main/java/ldts/t09g06/view/game/AmmoViewer.java
+++ gauntlet/src/main/java/ldts/t09g06/view/game/AmmoViewer.java
@@ -3,11 +3,12 @@ package ldts.t09g06.view.game;
 import ldts.t09g06.gui.GUI;
 import ldts.t09g06.model.game.elements.ammo.GenericAmmo;
 import ldts.t09g06.model.game.elements.monsters.GenericMonster;
+import ldts.t09g06.model.Position;
 
 public class AmmoViewer implements ElementViewer<GenericAmmo> {
     @Override
-    public void draw(GenericAmmo, GUI gui) {
-        gui.drawMonster(GenericAmmo.getPosition());
+    public void draw(GenericAmmo ammo, GUI gui) {
+        gui.drawMonster(ammo.getPosition());
     }
 }
 
