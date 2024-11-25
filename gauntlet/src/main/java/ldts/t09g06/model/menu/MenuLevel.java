d01c14e450112c74bfd21e6b3b4f31c3a01c343e
Andre Gomes <andregomes.ronfe@gmail.com>
2024-11-25 16:14:37 +0000

    levels done

---
+++ gauntlet/src/main/java/ldts/t09g06/model/menu/MenuLevel.java
@@ -0,0 +1,10 @@
+package ldts.t09g06.model.menu;
+
+import java.util.Arrays;
+
+public class MenuLevel extends GenericMenu {
+    public MenuLevel(){
+        super();
+        this.entries = Arrays.asList("Level 1", "Level 2", "Level 3", "Level 4", "Level 5");
+    }
+}
