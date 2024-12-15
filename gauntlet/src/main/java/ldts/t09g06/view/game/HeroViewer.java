dc7f66c55e532c8e6629ea3d5d8b8bc1fc33fc05
Valves734 <up202304912@up.pt>
2024-12-15 13:15:45 +0000

    hero changing direction

--- gauntlet/src/main/java/ldts/t09g06/view/game/HeroViewer.java
+++ gauntlet/src/main/java/ldts/t09g06/view/game/HeroViewer.java
@@ -2,21 +2,31 @@ package ldts.t09g06.view.game;
 
 
 import ldts.t09g06.gui.GUI;
+import ldts.t09g06.model.Direction;
 import ldts.t09g06.model.game.elements.heroes.Hero;
 import ldts.t09g06.view.Sprite;
 import ldts.t09g06.view.SpriteLoader;
 
 import java.io.IOException;
+import java.util.EnumMap;
 
 public class HeroViewer implements ElementViewer<Hero> {
-    private final Sprite sprite;
+    private final EnumMap<Direction, Sprite> sprites;
 
     public HeroViewer(SpriteLoader spriteLoader) throws IOException{
-        this.sprite = spriteLoader.get("sprites/hero/tile_0085.png");
+        this.sprites = new EnumMap<>(Direction.class);
+        this.sprites.put(Direction.UP, spriteLoader.get("sprites/hero/sprite_final_4.png"));
+        this.sprites.put(Direction.DOWN, spriteLoader.get("sprites/hero/down.png"));
+        this.sprites.put(Direction.LEFT, spriteLoader.get("sprites/hero/sprite_shitl.png"));
+        this.sprites.put(Direction.RIGHT, spriteLoader.get("sprites/hero/sprite_shitr.png"));
     }
 
     @Override
     public void draw(Hero hero, GUI gui) {
-        sprite.draw(gui,hero.getPosition().getX(),hero.getPosition().getY());
+        Direction direction = hero.getDirection();
+        Sprite sprite = sprites.get(direction);
+        if(sprite!=null) {
+            sprite.draw(gui, hero.getPosition().getX(), hero.getPosition().getY());
+        }
     }
 }
