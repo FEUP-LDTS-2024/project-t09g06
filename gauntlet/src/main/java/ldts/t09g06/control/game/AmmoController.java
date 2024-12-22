03ccaea52946fa853356d143ff881a0efd8f53d2
Valves734 <up202304912@up.pt>
2024-12-22 17:00:07 +0000

    fixed a bug where if the hero was next to a wall the boss bullets wouldnt hit the hero and optimized updateAmmo code

--- gauntlet/src/main/java/ldts/t09g06/control/game/AmmoController.java
+++ gauntlet/src/main/java/ldts/t09g06/control/game/AmmoController.java
@@ -27,78 +27,81 @@ public class AmmoController extends GameController {
 
     private void updateAmmo() {
         List<GenericAmmo> bulletsToRemove = new ArrayList<>();
+        List<GenericMonster> monstersToRemove = new ArrayList<>();
         List<Wall> walls = getModel().getWalls();
         List<GenericMonster> monsters = getModel().getMonsters();
         GenericMonster boss = getModel().getBoss();
         Hero hero = getModel().getHero();
-        List<GenericMonster> monstersToRemove = new ArrayList<>();
-
-        boolean collided = false;
 
         for (GenericAmmo bullet : getModel().getBullets()) {
-            if(bullet.isFromBoss()) {
-                Position nextPosition = getNextPosition(bullet);
+            Position nextPosition = getNextPosition(bullet);
+            boolean collided = false;
 
-                for (Wall wall : walls) {
-                    if (nextPosition.collidesWith(wall)) {
-                        bulletsToRemove.add(bullet);
-                        collided = true;
-                        break;
-                    }
+            if (bullet.isFromBoss()) {
+                if (bullet.collidesWith(hero)) {
+                    HeroHitByBossBullet(hero, bullet, bulletsToRemove);
+                    collided = true;
                 }
-                if (!collided){
-                    if (bullet.collidesWith(hero)) {
-                        AudioController.getInstance().playAudio(AudioOption.MONSTER_ATACK);
-                        getModel().getHero().decreaseLife(1);
-                        bulletsToRemove.add(bullet);
-                        collided = true;
-                        break;
-                    }
-                }
-                if (!collided) bullet.setPosition(nextPosition);
+                collided = WallCollision(bullet, nextPosition, walls, bulletsToRemove);
             }
-            else{
-                Position nextPosition = getNextPosition(bullet);
-
-                if (!collided ) {
-                    for (GenericMonster monster : monsters) {
-                        if (bullet.collidesWith(monster)) {
-                            AudioController.getInstance().playAudio(AudioOption.MONSTER_HIT);
-                            monstersToRemove.add(monster);
-                            bulletsToRemove.add(bullet);
-                            getModel().getHero().increase_score(10);
-                            collided = true;
-                            break;
-                        }
-                    }
-                    if (bullet.collidesWith(boss)) {
-                        AudioController.getInstance().playAudio(AudioOption.MONSTER_HIT);
-                        getModel().getBoss().decreaseLife(1);
-                        bulletsToRemove.add(bullet);
-                        if (getModel().getBoss().getLife() <= 0) {
-                            getModel().removeBoss(boss);
-                            getModel().getHero().increase_score(50);
-                        }
-                        collided = true;
-                        break;
-                    }
-
+            else {
+                collided = MonsterCollisions(bullet, monsters, monstersToRemove, bulletsToRemove, hero);
+                if (!collided && bullet.collidesWith(boss)) {
+                    BossCollision(bullet, boss, bulletsToRemove, hero);
+                    collided = true;
                 }
-                for (Wall wall : walls) {
-                    if (nextPosition.collidesWith(wall)) {
-                        bulletsToRemove.add(bullet);
-                        collided = true;
-                        break;
-                    }
+                if (!collided) {
+                    collided = WallCollision(bullet, nextPosition, walls, bulletsToRemove);
                 }
+            }
 
-
-                if (!collided) bullet.setPosition(nextPosition);
+            if (!collided) {
+                bullet.setPosition(nextPosition);
             }
         }
-        if(!bulletsToRemove.isEmpty()) getModel().removeBullets(bulletsToRemove);
-        if(!monstersToRemove.isEmpty())getModel().removeMonsters(monstersToRemove);
 
+
+        if (!bulletsToRemove.isEmpty()) getModel().removeBullets(bulletsToRemove);
+        if (!monstersToRemove.isEmpty()) getModel().removeMonsters(monstersToRemove);
+    }
+
+    private boolean WallCollision(GenericAmmo bullet, Position nextPosition, List<Wall> walls, List<GenericAmmo> bulletsToRemove) {
+        for (Wall wall : walls) {
+            if (nextPosition.collidesWith(wall)) {
+                bulletsToRemove.add(bullet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void HeroHitByBossBullet(Hero hero, GenericAmmo bullet, List<GenericAmmo> bulletsToRemove) {
+        AudioController.getInstance().playAudio(AudioOption.MONSTER_ATACK);
+        hero.decreaseLife(1);
+        bulletsToRemove.add(bullet);
+    }
+
+    private boolean MonsterCollisions(GenericAmmo bullet, List<GenericMonster> monsters, List<GenericMonster> monstersToRemove, List<GenericAmmo> bulletsToRemove, Hero hero) {
+        for (GenericMonster monster : monsters) {
+            if (bullet.collidesWith(monster)) {
+                AudioController.getInstance().playAudio(AudioOption.MONSTER_HIT);
+                monstersToRemove.add(monster);
+                bulletsToRemove.add(bullet);
+                hero.increase_score(10);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void BossCollision(GenericAmmo bullet, GenericMonster boss, List<GenericAmmo> bulletsToRemove, Hero hero) {
+        AudioController.getInstance().playAudio(AudioOption.MONSTER_HIT);
+        boss.decreaseLife(1);
+        bulletsToRemove.add(bullet);
+        if (boss.getLife() <= 0) {
+            getModel().removeBoss(boss);
+            hero.increase_score(50);
+        }
     }
 
     public Position getNextPosition(GenericAmmo ammo) {
